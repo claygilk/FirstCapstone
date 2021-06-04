909ef724e1c2792ede12ab486182cbaf6f3fce88
Jamie Mullins <janetzkej@gmail.com>
2021-06-04 13:09:17 -0400

    PairExercises

--- module-1_Mini-Capstone/CapstoneTests/AccountTests.cs
+++ module-1_Mini-Capstone/CapstoneTests/AccountTests.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CapstoneTests
 {
-    class AccountTests
+    [TestClass]
+    public class AccountTests
     {
+        [TestMethod]
+        public void Withdraw_CannotWithdrawNegativeNumber()
+        {
+            //Arrange
+            Account account = new Account();
+            decimal testWithdraw = -1;
+            //Act
+            decimal result = account.Withdraw(testWithdraw);
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Deposit_CannotDepositOver5000()
+        {
+            //Arrange
+            Account account = new Account();
+            decimal testDeposit = 50001;
+            //Act
+            decimal result = account.Deposit(testDeposit);
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void CompleteTransaction_()
+        {
+            //Arrange
+            Account account = new Account();
+            //decimal testCompleteTransaction = 5;
+            //Act
+            //decimal result = account.CompleteTransaction(testCompleteTransaction);
+            //Assert
+            //Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void GetChangeBack_()
+        {
+            //Arrange
+            Account account = new Account();
+            account.Deposit(1M);
+            account.Withdraw(0.50M);
+            string expected = $"Change Due: 0 - Twenties | 0 - Tens | 0 - Fives | 0 - Ones | 2 - Quarters | 0 - Dimes | 0 - Nickels";
+            //Act
+            string result = account.GetChangeBack();
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
--- module-1_Mini-Capstone/CapstoneTests/CateringItemTests.cs
+++ module-1_Mini-Capstone/CapstoneTests/CateringItemTests.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CapstoneTests
 {
-    class CateringItemTests
+    [TestClass]
+    public class CateringItemTests
     {
+        [TestMethod]
+        public void SellItem_InsufficientStock_ReturnsFalse()
+        {
+            //Arrange
+            CateringItem item = new CateringItem();
+            item.InStock = 10;
+            //Act
+            bool actual = item.SellItem(11);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod]
+        public void SellItem_NegativeQuantity_ReturnsFalse()
+        {
+            //Arrange
+            CateringItem item = new CateringItem();
+            item.InStock = 10;
+            //Act
+            bool actual = item.SellItem(-5);
+            //Assert
+            Assert.IsFalse(actual);
+        }
     }
 }
